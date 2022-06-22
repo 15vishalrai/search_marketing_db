5311c4d0c78175c4f3f1cf246ee90a995bc5ae44
abc <abc@example.com>
2022-06-22 11:02:27 +0530

    search market db

---
+++ src/main/java/com/search_marketing_db/SearchMarketingDb1Application.java
@@ -0,0 +1,13 @@
+package com.search_marketing_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchMarketingDb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchMarketingDb1Application.class, args);
+	}
+
+}
